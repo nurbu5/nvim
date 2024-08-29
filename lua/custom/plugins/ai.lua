367c294836ce662bba18e5e482274c16ef551e18
Dawa Sherpa <dawansherpa5@gmail.com>
2024-08-28 22:40:19 -0600

    Update copilot settings

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -1,28 +1,41 @@
 return {
   "zbirenbaum/copilot.lua",
-  enabled = false,
   cmd = "Copilot",
   build = ":Copilot auth",
-  opts = {
-    panel = {
-      enabled = true,
-      auto_refresh = false,
-      keymap = {
-        jump_prev = "p",
-        jump_next = "n",
-        accept = "<CR>",
-        refresh = "gr",
-        open = "<C-a>"
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      server_opts_overrides = {
+        trace = "verbose",
+        settings = {
+          advanced = {
+            listCount = 5, -- #completions for panel (default 10)
+            inlineSuggestCount = 3, -- #completions for getCompletions (default 3)
+          }
+        },
       },
-      layout = {
-        position = "bottom", -- | top | left | right
-        ratio = 0.4
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "N",
+          jump_next = "n",
+          accept = "<CR>",
+          refresh = "<C-r>",
+          open = "<C-s>"
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.5
+        },
       },
-    },
-    suggestion = { enabled = false, },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
-  },
+      suggestion = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+      },
+    })
+  end,
 }
